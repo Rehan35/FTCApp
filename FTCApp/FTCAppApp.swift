9231eca2f05b29e4365d8ede05d60b5d527d65b6
Rehan Parwani <rehanparwani@gmail.com>
2021-09-16 18:26:50 -0700

    Initial Commit

---
+++ FTCApp/FTCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTCAppApp.swift
+//  FTCApp
+//
+//  Created by Rehan Parwani on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FTCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
