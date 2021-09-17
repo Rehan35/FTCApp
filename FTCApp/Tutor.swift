1586b602a82c1da2b956dbb4c8e098db59c5cc3a
Rehan Parwani <rehanparwani@gmail.com>
2021-09-16 20:36:32 -0700

    student List added

--- FTCApp/Tutor.swift
+++ FTCApp/Tutor.swift
@@ -31,7 +31,6 @@ class Tutor: ObservableObject, Identifiable{
         if requiresEnglish{
             subjects.append("English")
         }
-        
         self.numberOfHours = numberOfHours
     }
     
