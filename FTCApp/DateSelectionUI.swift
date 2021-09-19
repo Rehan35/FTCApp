471a74ac970daffce616a2e2f6ebc7cece8626ff
Rehan Parwani <rehanparwani@gmail.com>
2021-09-19 15:19:37 -0700

    For Each

--- FTCApp/DateSelectionUI.swift
+++ FTCApp/DateSelectionUI.swift
@@ -11,10 +11,12 @@ struct CheckBoxView: View {
     @State var checked: Bool = false
 
     var body: some View {
-        Image(systemName: checked ? "checkmark.square.fill" : "square")
-            .foregroundColor(checked ? Color(UIColor.systemBlue) : Color.secondary)
-            .onTapGesture {
-                self.checked.toggle()
-            }
+        ForEach(1..<8){_ in
+            Image(systemName: checked ? "checkmark.square.fill" : "square")
+                .foregroundColor(checked ? Color(UIColor.systemBlue) : Color.secondary)
+                .onTapGesture {
+                    self.checked.toggle()
+                }
+        }
     }
 }
